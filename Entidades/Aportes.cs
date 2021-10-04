0e7cbe7f62e92591917646b2935b91e1177ce4a2
fabie <fabie@DESKTOP-ILTFSL4>
2021-10-04 19:40:59 -0400

    Creando la entidad Aportes

---
+++ Entidades/Aportes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_AP1_Prandi_20190281.Entidades
+{
+    public class Aportes
+    {
+        [Key]
+        public int AporteID { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Persona { get; set; }
+        public string Concepto { get; set; }
+        public float Monto { get; set; }
+
+    }
+}
