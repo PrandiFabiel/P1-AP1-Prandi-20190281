b1eaca9270f225879de5be56f69aecab3a35cd2f
fabie <fabie@DESKTOP-ILTFSL4>
2021-10-04 19:54:28 -0400

    agregando la migracion y poniendo la propiedad copy aways

---
+++ Migrations/20211004235127_MigracionInicial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P1_AP1_Prandi_20190281.Migrations
+{
+    public partial class MigracionInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aportes",
+                columns: table => new
+                {
+                    AporteID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Persona = table.Column<string>(type: "TEXT", nullable: true),
+                    Concepto = table.Column<string>(type: "TEXT", nullable: true),
+                    Monto = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aportes", x => x.AporteID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aportes");
+        }
+    }
+}
